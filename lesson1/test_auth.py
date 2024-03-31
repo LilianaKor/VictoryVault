5ee2f389bbec622c079a69134566f3794402b378
Liliana <lilianakoric@gmail.com>
2024-03-30 19:01:37 -0700

    Add Tests: Auth, Add_to_cart; Transition_to_cart

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -1,5 +1,7 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
+import pytest
+import time
 
 browser = webdriver.Chrome()
 
@@ -11,9 +13,18 @@ def test_auth_positive():
     browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
     assert browser.current_url == 'https://www.saucedemo.com/v1/inventory.html', 'url не соответствует ожидаемому'
 
-
     browser.quit()
 
+def test_auth_negative():
+    browser.get('https://www.saucedemo.com/')
 
+    browser.find_element('xpath', '//*[@id="user-name"]').send_keys('no_user')
+    browser.find_element(By.XPATH, '//*[@id="password"]').send_keys('secret_sauce')
+    browser.find_element(By.XPATH, '//*[@id="login-button"]').click()
+    error_message = browser.find_element(By.XPATH, '//div[@class = "error-message-container error"]')
+    assert error_message.is_displayed()
+    print(error_message.text)
+
+    browser.quit()
 
 
