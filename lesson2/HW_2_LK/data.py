2f468cc9f2e386ddae864571be020c0fe9aee85f
Liliana <lilianakoric@gmail.com>
2024-04-06 18:19:16 -0700

    Add conftest.py, data.py, locators.py

---
+++ lesson2/HW_2_LK/data.py
@@ -0,0 +1,38 @@
+LOGIN = 'standard_user'
+PASSWORD = 'secret_sauce'
+
+# URLS
+MAIN_PAGE = 'https://www.saucedemo.com/'
+
+
+
+
+# URLS
+# main_url = ‘https://www.saucedemo.com/’
+# catalog_url = f’{main_url}inventory.html’
+# card_url = f’{main_url}inventory-item.html?id=4'
+# complete_order_url = f’{main_url}checkout-complete.html’
+# about_url = ‘https://saucelabs.com/’
+# # DATA
+# # auth
+# username_pos = “standard_user”
+# password_pos = “secret_sauce”
+# # texts
+# error_auth_neg = “Epic sadface: Username and password do not match any user in this service”
+# order_pos_text = “Thank you for your order!”
+# # value
+# # number_in_cart_value = “1”
+# select_az = “az”
+# select_za = “za”
+# select_lohi = “lohi”
+# select_hilo = “hilo”
+# # --------------------------------------------------------------------------------
+# # --------------------------------------------------------------------------------
+# register_form_url = “https://victoretc.github.io/webelements_information/”
+#
+# saucedemo.comsaucedemo.com
+# Swag Labs
+# Sauce Labs Swag Labs app
+#
+# saucelabs.comsaucelabs.com
+# Sauce Labs: Cross Browser Testing, Selenium Testing & Mobile Testing
---
+++ lesson2/HW_2_LK/locators.py
@@ -0,0 +1,4 @@
+
+USERNAME_FIELD = '//input[@data-test="username"]'
+PASSWORD_FIELD = '//input[@data-test="password"]'
+LOGIN_BUTTON = '//input[@data-test="login-button"]'
