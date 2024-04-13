4ea93df24c63493b057a2eeaebbbadb82dfdaf79
Liliana <lilianakoric@gmail.com>
2024-04-13 16:36:54 -0700

    Add def test_auth_transition_to_cart_by_name():     login()

--- lesson1/test_auth.py
+++ lesson1/test_auth.py
@@ -2,8 +2,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 browser = webdriver.Chrome()
+
+MENU = (By.CSS_SELECTOR, )
+# ABOUT = (By.)
+# ABOUT_TEXT = (By.)
+LOGOUT_MENU = (By.ID, '')
+RESET_APP_STATE = (By.ID, '')
+ADD_TO_CART_BTN = (By.CSS_SELECTOR, '')
+
+# CART_BADGE_WITH_ITEM = (By.)
+# CART_BADGE_EMPTY = (By.)
+
+
+driver_path = ChromeDriverManager().install()
+
+#create a new Chrome browser instance
+service = Service(driver_path)
+browser = webdriver. Chrome(service=service)
+browser.maximize_window()
+browser.implicitly_wait(5)
+wait = WebDriverWait(browser,10)
 
 def test_auth_positive():
     browser.get('https://www.saucedemo.com/v1/')
@@ -26,6 +50,7 @@ def test_auth_negative():
     print(error_message.text)
 
 
+     # #  create login function
 def login():
     browser.get('https://www.saucedemo.com/')
     browser.implicitly_wait(10)
